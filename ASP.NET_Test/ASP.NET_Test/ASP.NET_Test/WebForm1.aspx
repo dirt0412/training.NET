360b814b54efb755c41e5e51644f8bbec636f8e3
dirt0412 <dirt@poczta.gazeta.pl>
2015-09-24 01:00:53 +0200

    new

---
+++ ASP.NET_Test/ASP.NET_Test/ASP.NET_Test/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASP.NET_Test.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label6" runat="server">czas: </asp:Label>
+&nbsp;<asp:Label ID="Label5" runat="server" Font-Bold="True"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Zapisz" Width="85px" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Pierwsze wywolanie strony:"></asp:Label>
+&nbsp;
+        <asp:Label ID="Label2" runat="server" Font-Bold="True" ForeColor="Red" Text="-"></asp:Label>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Kolejne wywolanie strony:"></asp:Label>
+&nbsp;&nbsp;
+        <asp:Label ID="Label4" runat="server" Font-Bold="True" ForeColor="Red" Text="-"></asp:Label>
+&nbsp;</div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET_Test/ASP.NET_Test/ASP.NET_Test/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_Test
+{
+  public partial class WebForm1 : System.Web.UI.Page
+  {
+    protected override void OnPreRender (EventArgs e)
+    {
+      if(ViewState["valueFirstLoad"]!=null)
+      Label2.Text = ViewState["valueFirstLoad"].ToString();
+      if (ViewState["valueSecondLoad"] != null)
+        Label4.Text = ViewState["valueSecondLoad"].ToString();
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      Label5.Text = DateTime.Now.ToString("HH:mm:ss");
+      if (!Page.IsPostBack)//pierwsze wywolanie strony
+      {
+        ViewState["valueFirstLoad"] = Label5.Text;
+        Label2.Text = ViewState["valueFirstLoad"].ToString();
+      }
+      else
+      {//kolejne wywolanie strony
+        ViewState["valueSecondLoad"] = Label5.Text;
+        Label4.Text = ViewState["valueSecondLoad"].ToString();
+      }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+      //ViewState["valueFirstLoad"] = TextBox1.Text;
+      //ViewState["valueSecondLoad"] = TextBox1.Text;
+    }
+  }
+}
