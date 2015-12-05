72cf0d7a1c658bb72c2a924c55d30b338ed4a5ba
robertdrwecki@gmail.com <robertdrwecki@gmail.com>
2015-12-05 22:03:21 +0100

    delete asp.net

--- MasterPageTest/MasterPageTest/Default.aspx
+++ MasterPageTest/MasterPageTest/Default.aspx
@@ -11,5 +11,21 @@
     </asp:DropDownList>
     
     <asp:Button ID="Button1" runat="server" Text="Button" />
-    
+   
+    <div>    
+        <asp:Label ID="Label6" runat="server">czas: </asp:Label>
+&nbsp;<asp:Label ID="Label5" runat="server" Font-Bold="True"></asp:Label>
+        <br />
+        <%--<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>--%>
+&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Refresh ViewState" Width="130px" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Pierwsze wywolanie strony:"></asp:Label>
+&nbsp;
+        <asp:Label ID="Label2" runat="server" Font-Bold="True" ForeColor="Red" Text="-"></asp:Label>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Kolejne wywolanie strony:"></asp:Label>
+&nbsp;&nbsp;
+        <asp:Label ID="Label4" runat="server" Font-Bold="True" ForeColor="Red" Text="-"></asp:Label>
+&nbsp;</div>
+   
 </asp:Content>
--- MasterPageTest/MasterPageTest/Default.aspx.cs
+++ MasterPageTest/MasterPageTest/Default.aspx.cs
@@ -7,44 +7,73 @@ using System.Web.UI.WebControls;
 
 namespace MyMasterPage
 {
-  public partial class Default : System.Web.UI.Page
-  {
-    /// <summary>
-    /// kolejnosc (1)
-    /// startuje na poczatku 
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    protected void Page_PreInit(object sender, EventArgs e)
+    public partial class Default : System.Web.UI.Page
     {
-      if (Session["master"] != null)
-      {
-        MasterPageFile = (string)Session["master"];
-      }
-    }
+        /// <summary>
+        /// kolejnosc (1)
+        /// startuje na poczatku 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (Session["master"] != null)
+            {
+                MasterPageFile = (string)Session["master"];
+            }
+        }
 
-    /// <summary>
-    /// kolejnosc (2)
-    /// wyszukiwanie kontrolki i przypisanie jej do zmiennej MasterPage
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    protected void Page_Load(object sender, EventArgs e)
-    {
-      ((Label)Master.FindControl("Label1")).Text = this.TextBox1.Text;
-    }
+        /// <summary>
+        /// kolejnosc (2)
+        /// wyszukiwanie kontrolki i przypisanie jej do zmiennej MasterPage
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ((Label)Master.FindControl("Label1")).Text = this.TextBox1.Text;
 
-    /// <summary>
-    /// kolejnosc (4) ewentualnie w przypadku użycia
-    /// wykorzystanie mechanizmu sesji do przekazywania danych miedzy poszczegolnymi wywolaniami strony
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-      Session["master"] = this.DropDownList1.SelectedValue;
-      Response.Redirect(Request.RawUrl);//przeladowanie całej strony
-    }
+            Label5.Text = DateTime.Now.ToString("HH:mm:ss");
 
-  }
+            if (!Page.IsPostBack)//pierwsze wywolanie strony
+            {
+                ViewState["valueFirstLoad"] = Label5.Text;
+                if (Session["master"] != null)
+                    this.DropDownList1.SelectedValue = Session["master"].ToString();
+            }
+            else
+            {//kolejne wywolania strony
+                ViewState["valueSecondLoad"] = Label5.Text;
+                Session["master"] = this.DropDownList1.SelectedValue;                
+            }
+        }
+
+        /// <summary>
+        /// kolejnosc (4) ewentualnie w przypadku użycia
+        /// wykorzystanie mechanizmu sesji do przekazywania danych miedzy poszczegolnymi wywolaniami strony
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["master"] = this.DropDownList1.SelectedValue;
+            Response.Redirect(Request.RawUrl);//przeladowanie całej strony
+        }
+
+        /// <summary>
+        /// mechanizm ViewState
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (ViewState["valueFirstLoad"] != null)
+                Label2.Text = ViewState["valueFirstLoad"].ToString();
+
+            if (ViewState["valueSecondLoad"] != null)
+                Label4.Text = ViewState["valueSecondLoad"].ToString();
+
+        }
+
+    }
 }
